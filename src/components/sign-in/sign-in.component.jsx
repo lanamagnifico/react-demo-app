935776059a9390cde8ca95b5dedcaa00e7810767
lanamagnifico <svetlana.magnifico@gmail.com>
2020-02-24 12:37:36 +0100

    Add form component and sign in page

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({
+            email: '',
+            password: ''
+        });
+    }
+
+    handelChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+
+                    <FormInput
+                        type="email"
+                        name="email"
+                        value={this.state.email}
+                        label="email"
+                        required
+                        handleChange={this.handelChange} />
+
+                    <FormInput
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        label="password"
+                        required
+                        handleChange={this.handelChange} />
+
+                    <CustomButton type="submit">
+                        SIGN IN
+                        </CustomButton>
+
+                </form>
+
+            </div>
+        )
+    }
+}
+
+export default SignIn;
